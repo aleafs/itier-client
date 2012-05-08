dbe99a93138c273f0b63b0d3b6c41edb75e6ef4a
aleafs <zhangxc83@gmail.com>
2012-05-08 18:09:46 +0800

    replace tab to 2 spaces

--- test/http-client-test.js
+++ test/http-client-test.js
@@ -1,181 +1,187 @@
 /* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
 
 if (process.env.JSCOV) {
-    var jscover = require('jscoverage');
-    require = jscover.require(module);
+  var jscover = require('jscoverage');
+  require = jscover.require(module);
 }
 
 var should  = require('should');
 var Client  = require(__dirname + '/../lib/http-client.js', true);
 var http = require('http');
 
+/* {{{ http server handle() */
 function handle(req, res) {
-    var body = '';
-    req.on('data', function(buf) {
-        body += buf.toString();
-    });
-    req.on('end', function() {
-        if (req.url.indexOf('/timeout') >= 0) {
-            setTimeout(function() {
-                res.writeHead(502, {});
-                res.end('200');
-            }, 300);
-        } else {
-            res.writeHead(200, {
-                'x-header-1'    : 'a',
-                'X-header-2'    : 'B',
-            });
-            res.end(JSON.stringify({
-                'url'   : req.url,
-                'header': req.headers,
-                'method': req.method,
-                'post'  : body,
-            }));
-        }
-    });
+  var body = '';
+  req.on('data', function(buf) {
+    body += buf.toString();
+  });
+  req.on('end', function() {
+    if (req.url.indexOf('/timeout') >= 0) {
+      setTimeout(function() {
+        res.writeHead(502, {});
+        res.end('200');
+      }, 300);
+    } else {
+      res.writeHead(200, {
+        'x-header-1'    : 'a',
+        'X-header-2'    : 'B',
+      });
+      res.end(JSON.stringify({
+        'url'   : req.url,
+        'header': req.headers,
+        'method': req.method,
+        'post'  : body,
+      }));
+    }
+  });
 }
+/* }}} */
 
 /* {{{ http service demo for unittest */
 var server1 = http.createServer(handle).listen(33749)
-var server2 = http.createServer(handle).listen(33748);
-var server3 = http.createServer(handle).listen(33747);
-/* }}} */
+  var server2 = http.createServer(handle).listen(33748);
+  var server3 = http.createServer(handle).listen(33747);
+  /* }}} */
 
-describe('http-client-test', function() {
+  describe('http-client-test', function() {
 
     /* {{{ should_http_client_get_and_post_works_fine() */
     it('should_http_client_get_and_post_works_fine', function(done) {
-        var client  = Client.create({
-            'prefix'    : '/a',
-        });
-        var count   = 2;
+      var client  = Client.create({
+        'prefix'    : '/a',
+      });
+      var count   = 2;
 
-        client.bind('127.0.0.1', 33749);
-        client.get('/status?a=1.23456&b=B', function(error, data, code, header) {
-            code.should.eql(200);
-            header['x-header-1'].should.eql('a');
-            header['x-header-2'].should.eql('B');
+      client.bind('127.0.0.1', 33749);
+      client.get('/status?a=1.23456&b=B', function(error, data, code, header) {
+        code.should.eql(200);
+        header['x-header-1'].should.eql('a');
+        header['x-header-2'].should.eql('B');
 
-            data    = JSON.parse(data);
+        data    = JSON.parse(data);
 
-            data.url.should.include('/a/status?a=1.23456&b=B');
-            data.method.should.eql('GET');
-            data.header['x-my-header'].should.eql('asdf');
-            data.header.should.not.have.property('post');
-            data.header.should.not.have.property('content-length');
+        data.url.should.include('/a/status?a=1.23456&b=B');
+        data.method.should.eql('GET');
+        data.header['x-my-header'].should.eql('asdf');
+        data.header.should.not.have.property('post');
+        data.header.should.not.have.property('content-length');
 
-            if ((--count) == 0) {
-                done();
-            }
-        }, {'x-my-hEader' : 'asdf'}).post('/status?a=1.23456&b=B', {
-            'c1'    : 'C...1',
-            'c2'    : [1, 2, 3]
-        }, function(error, data, code, header) {
-            data    = JSON.parse(data);
+        if ((--count) == 0) {
+          done();
+        }
+      }, {'x-my-hEader' : 'asdf'}).post('/status?a=1.23456&b=B', {
+        'c1'    : 'C...1',
+        'c2'    : [1, 2, 3]
+      }, function(error, data, code, header) {
+        data    = JSON.parse(data);
 
-            data.url.should.include('/a/status?a=1.23456&b=B');
-            data.method.should.eql('POST');
-            data.post.should.eql(JSON.stringify({
-                'c1'    : 'C...1',
-                'c2'    : [1, 2, 3]
-            }));
-            if ((--count) == 0) {
-                done();
-            }
-        });
+        data.url.should.include('/a/status?a=1.23456&b=B');
+        data.method.should.eql('POST');
+        data.post.should.eql(JSON.stringify({
+          'c1'    : 'C...1',
+          'c2'    : [1, 2, 3]
+        }));
+        if ((--count) == 0) {
+          done();
+        }
+      });
     });
     /* }}} */
 
     /* {{{ should_set_error_handle_works_fine() */
     it('should_set_error_handle_works_fine', function(done) {
-        var client  = Client.create();
-        client.get('/a', function(error, data, code, header) {
-            error.number.should.eql(1000);
-            error.name.should.equal('BadServer');
-            error.toString().should.include('Empty server list or bad server id');
-            client.bind('127.0.0.1', 11).get('/a', function(error, data) {
-                error.number.should.eql(1200);
-                error.toString().should.include('connect ECONNREFUSE');
-                done();
-            });
+      var client  = Client.create();
+      client.get('/a', function(error, data, code, header) {
+        error.number.should.eql(1000);
+        error.name.should.equal('BadServer');
+        error.toString().should.include('Empty server list or bad server id');
+        client.bind('127.0.0.1', 11).get('/a', function(error, data) {
+          error.number.should.eql(1200);
+          error.toString().should.include('connect ECONNREFUSE');
+          done();
         });
+      });
     });
     /* }}} */
 
     /* {{{ should_http_request_timeout_works_fine() */
     it('should_http_request_timeout_works_fine', function(done) {
-        var client  = Client.create({ timeout: 200 });
-        client.bind('127.0.0.1', 33748);
-        client.get('/timeout', function(err, data, code, header) {
-            should.exist(err);
-            err.name.should.equal('RequestTimeout');
-            err.message.should.include('Request Timeout 300ms');
-            err.code.should.equal('ECONNRESET');
-            setTimeout(function() {
-                done();
-            }, 500)
-        });
+      var client  = Client.create({ timeout: 200 });
+      client.bind('127.0.0.1', 33748);
+      client.get('/timeout', function(err, data, code, header) {
+        should.exist(err);
+        err.name.should.equal('RequestTimeout');
+        err.message.should.include('Request Timeout 300ms');
+        err.code.should.equal('ECONNRESET');
+        setTimeout(function() {
+          done();
+        }, 500)
+      });
     });
     /* }}} */
 
     /* {{{ should_http_walk_works_fine() */
     it('should_http_walk_works_fine', function(done) {
-        var client  = Client.create();
-        var count   = 2;
-        client.bind('127.0.0.1', 33748).bind('127.0.0.1', 33749);
-        client.walk('/walk', null, function(error, data, code, header) {
-            should.not.exist(error);
-            if ((--count) < 1) {
-                done();
-            }
-        });
+      var client  = Client.create();
+      var count   = 2;
+      client.bind('127.0.0.1', 33748).bind('127.0.0.1', 33749);
+      client.walk('/walk', null, function(error, data, code, header) {
+        should.not.exist(error);
+        if ((--count) < 1) {
+          done();
+        }
+      });
     });
     /* }}} */
 
+    /* {{{ should_request_many_ports_works_fine() */
     it('should request :33748 => :33749 => :33747 => :33748', function(done) {
-        var client = Client.create();
-        client.bind('127.0.0.1', 33748).bind('127.0.0.1', 33749).bind('127.0.0.1', 33747);
+      var client = Client.create();
+      client.bind('127.0.0.1', 33748).bind('127.0.0.1', 33749).bind('127.0.0.1', 33747);
+      client.post('/post', { a: '123' }, function(err, data, code, headers) {
+        should.not.exist(err);
+        var r = JSON.parse(data);
+        r.header.host.should.equal('127.0.0.1:33748');
         client.post('/post', { a: '123' }, function(err, data, code, headers) {
+          should.not.exist(err);
+          var r = JSON.parse(data);
+          r.header.host.should.equal('127.0.0.1:33749');
+          client.post('/post', { a: '123' }, function(err, data, code, headers) {
             should.not.exist(err);
             var r = JSON.parse(data);
-            r.header.host.should.equal('127.0.0.1:33748');
+            r.header.host.should.equal('127.0.0.1:33747');
             client.post('/post', { a: '123' }, function(err, data, code, headers) {
-                should.not.exist(err);
-                var r = JSON.parse(data);
-                r.header.host.should.equal('127.0.0.1:33749');
-                client.post('/post', { a: '123' }, function(err, data, code, headers) {
-                    should.not.exist(err);
-                    var r = JSON.parse(data);
-                    r.header.host.should.equal('127.0.0.1:33747');
-                    client.post('/post', { a: '123' }, function(err, data, code, headers) {
-                        should.not.exist(err);
-                        var r = JSON.parse(data);
-                        r.header.host.should.equal('127.0.0.1:33748');
-                        done();
-                    });
-                });
+              should.not.exist(err);
+              var r = JSON.parse(data);
+              r.header.host.should.equal('127.0.0.1:33748');
+              done();
             });
+          });
         });
+      });
     });
+    /* }}} */
 
+    /* {{{ should_post_with_content_length_works_fine() */
     it('should post with Content-Length', function(done) {
-        var client  = Client.create();
-        client.bind('127.0.0.1', 33748).bind('127.0.0.1', 33749);
-        var post = {"a":"b"};
-        client.post('/post', post, function(err, data, code, header) {
-            should.not.exist(err);
-            code.should.equal(200);
-            data = JSON.parse(data);
-            data.header['content-length'].should.equal(''+JSON.stringify(post).length);
-            done();
-        });
+      var client  = Client.create();
+      client.bind('127.0.0.1', 33748).bind('127.0.0.1', 33749);
+      var post = {"a":"b"};
+      client.post('/post', post, function(err, data, code, header) {
+        should.not.exist(err);
+        code.should.equal(200);
+        data = JSON.parse(data);
+        data.header['content-length'].should.equal(''+JSON.stringify(post).length);
+        done();
+      });
     });
+    /* }}} */
 
-});
+  });
 
 after(function() {
-    server1.close();
-    server2.close();
-    server3.close();
+  server1.close();
+  server2.close();
+  server3.close();
 });
 
--- test/itier-client-test.js
+++ test/itier-client-test.js
@@ -1,9 +1,9 @@
 /* vim: set expandtab tabstop=2 shiftwidth=2 foldmethod=marker: */
 
 if (process.env.JSCOV) {
-    var jscover = require('jscoverage');
-    require = jscover.require(module);
-    process.on('exit', jscover.coverage);
+  var jscover = require('jscoverage');
+  require = jscover.require(module);
+  process.on('exit', jscover.coverage);
 }
 
 var should  = require('should');
@@ -11,161 +11,161 @@ var ITier   = require(__dirname + '/../', true);
 
 /* {{{ mock itier service on 33750 */
 var HTTP    = require('http').createServer(function(req, res) {
-    if (req.headers['x-app-name'] == 'denied') {
-        res.writeHead(401, {'WWW-Authenticate' : 'Basic realm="."'});
-        res.end('Authenticate denied for "' + req.headers['x-app-name'] + '"');
-        return;
+  if (req.headers['x-app-name'] == 'denied') {
+    res.writeHead(401, {'WWW-Authenticate' : 'Basic realm="."'});
+    res.end('Authenticate denied for "' + req.headers['x-app-name'] + '"');
+    return;
+  }
+  var data = '';
+  req.on('data', function(buf) {
+    data += buf.toString();
+  });
+  req.on('end', function() {
+    if (data.indexOf('hbase.t404') > 0) {
+      var hbase404 = '{"v":"1.0","c":400,"m":"hbase status code error: 404","t":0,"n":0,"fn":0,"f":[],"d":[]}';
+      return res.end(hbase404);
     }
-    var data = '';
-    req.on('data', function(buf) {
-        data += buf.toString();
-    });
-    req.on('end', function() {
-        if (data.indexOf('hbase.t404') > 0) {
-            var hbase404 = '{"v":"1.0","c":400,"m":"hbase status code error: 404","t":0,"n":0,"fn":0,"f":[],"d":[]}';
-            return res.end(hbase404);
-        }
 
-        if (data.indexOf('objectErrorMessage') > 0) {
-            var error = '{"v":"1.0","c":400,"m":{},"t":0,"n":0,"fn":0,"f":[],"d":[]}';
-            return res.end(error);
-        }
+    if (data.indexOf('objectErrorMessage') > 0) {
+      var error = '{"v":"1.0","c":400,"m":{},"t":0,"n":0,"fn":0,"f":[],"d":[]}';
+      return res.end(error);
+    }
 
-        if (data.indexOf('id in (:id)') > 0) {
-            var ret = {
-                'v' : '1.0',                
-                'c' : 200,                  
-                'm' : 'status ok',       
-                't' : 2,           
-                'n' : 2,          
-                'fn': 2,        
-                'f' : ['c1'],   
-                'd' : [[JSON.parse(data)]], 
-            };
-            return res.end(JSON.stringify(ret));
-        }
+    if (data.indexOf('id in (:id)') > 0) {
+      var ret = {
+        'v' : '1.0',                
+    'c' : 200,                  
+    'm' : 'status ok',       
+    't' : 2,           
+    'n' : 2,          
+    'fn': 2,        
+    'f' : ['c1'],   
+    'd' : [[JSON.parse(data)]], 
+      };
+      return res.end(JSON.stringify(ret));
+    }
 
-        var ret = {
-            'v' : '1.0',                /**<    数据格式版本号  */
-            'c' : 200,                  /**<    请求返回码      */
-            'm' : 'status ok',          /**<    响应消息        */
-            't' : 2,                    /**<    数据总行数      */
-            'n' : 2,                    /**<    此次请求返回总行数  */
-            'fn': 2,                    /**<    字段数  */
-            'f' : ['c1', 'c2'],         /**<    字段名  */
-            'd' : [[1,2],[3,4]],        /**<    数据体  */
-        };
+    var ret = {
+      'v' : '1.0',                /**<    数据格式版本号  */
+      'c' : 200,                  /**<    请求返回码      */
+      'm' : 'status ok',          /**<    响应消息        */
+      't' : 2,                    /**<    数据总行数      */
+      'n' : 2,                    /**<    此次请求返回总行数  */
+      'fn': 2,                    /**<    字段数  */
+      'f' : ['c1', 'c2'],         /**<    字段名  */
+      'd' : [[1,2],[3,4]],        /**<    数据体  */
+    };
 
-        var headers = {
-            'Content-Type'  : 'text/plain',
-        };
-        if ('x-itier-expire' in req.headers) {
-            ret.d.push([100, req.headers['x-itier-expire']]);
-        }
-        res.writeHead(200, headers);
+    var headers = {
+      'Content-Type'  : 'text/plain',
+    };
+    if ('x-itier-expire' in req.headers) {
+      ret.d.push([100, req.headers['x-itier-expire']]);
+    }
+    res.writeHead(200, headers);
 
-        res.end(JSON.stringify(ret));
-    });
+    res.end(JSON.stringify(ret));
+  });
 }).listen(33750);
 /* }}} */
 
 describe('itier-client-test', function() {
-    var client = null;
-    before(function() {
-        client = ITier.createClient();
-        client.connect('127.0.0.1', 33750);
-    });
+  var client = null;
+  before(function() {
+    client = ITier.createClient();
+    client.connect('127.0.0.1', 33750);
+  });
 
-    /* {{{ should_select_data_from_itier_works_fine() */
-    it('should_select_data_from_itier_works_fine', function(done) {
-        client.query('SELECT * FROM myfox.table_info', null, function(error, data, header, profile) {
-            data.should.eql([{'c1':1,'c2':2},{'c1':3,'c2':4}]);
-            profile.should.eql([]);
-            header.should.eql({
-                'version'   : '1.0',
-                'status'    : 200,
-                'expire'    : -1,
-                'message'   : 'status ok',
-                'row_num'   : 2,
-                'column_num': 2
-            });
-            done();
-        });
+  /* {{{ should_select_data_from_itier_works_fine() */
+  it('should_select_data_from_itier_works_fine', function(done) {
+    client.query('SELECT * FROM myfox.table_info', null, function(error, data, header, profile) {
+      data.should.eql([{'c1':1,'c2':2},{'c1':3,'c2':4}]);
+      profile.should.eql([]);
+      header.should.eql({
+        'version'   : '1.0',
+        'status'    : 200,
+        'expire'    : -1,
+        'message'   : 'status ok',
+        'row_num'   : 2,
+        'column_num': 2
+      });
+      done();
     });
-    /* }}} */
+  });
+  /* }}} */
 
-    /* {{{ should_fetch_mode_equal_array_works_fine() */
-    it('should_fetch_mode_equal_array_works_fine', function(done) {
-        var itier   = ITier.createClient({
-            'fetchmode' : ITier.FETCH.ARRAY,
-        });
-        itier.connect('127.0.0.1', 33750);
-        itier.query('SELECT * FROM myfox.table_info', null, function(error, data, header, profile) {
-            data.should.eql([[1,2],[3,4]]);
-            header.columns.should.eql(['c1','c2']);
-            done();
-        });
+  /* {{{ should_fetch_mode_equal_array_works_fine() */
+  it('should_fetch_mode_equal_array_works_fine', function(done) {
+    var itier   = ITier.createClient({
+      'fetchmode' : ITier.FETCH.ARRAY,
     });
-    /* }}} */
+    itier.connect('127.0.0.1', 33750);
+    itier.query('SELECT * FROM myfox.table_info', null, function(error, data, header, profile) {
+      data.should.eql([[1,2],[3,4]]);
+      header.columns.should.eql(['c1','c2']);
+      done();
+    });
+  });
+  /* }}} */
 
-    /* {{{ should_appname_authorize_works_fine() */
-    it('should_appname_authorize_works_fine', function(done) {
-        var itier   = ITier.createClient({
-            'appname'   : 'denied',
-        });
-        itier.connect('127.0.0.1', 33750);
-        itier.query('SHOW TABLES', null, function(error, data, header, profile) {
-            error.message.should.equal('HTTP 401 Response');
-            error.body.should.equal('Authenticate denied for "denied"');
-            done();
-        });
+  /* {{{ should_appname_authorize_works_fine() */
+  it('should_appname_authorize_works_fine', function(done) {
+    var itier   = ITier.createClient({
+      'appname'   : 'denied',
     });
-    /* }}} */
+    itier.connect('127.0.0.1', 33750);
+    itier.query('SHOW TABLES', null, function(error, data, header, profile) {
+      error.message.should.equal('HTTP 401 Response');
+      error.body.should.equal('Authenticate denied for "denied"');
+      done();
+    });
+  });
+  /* }}} */
 
-    it('should return [] when hbase 404', function(done) {
-        client.query('select * from hbase.t404 where row = :r', { r: 123 }, function(err, rows) {
-            should.not.exist(err);
-            rows.should.length(0);
-            done();
-        });
+  it('should return [] when hbase 404', function(done) {
+    client.query('select * from hbase.t404 where row = :r', { r: 123 }, function(err, rows) {
+      should.not.exist(err);
+      rows.should.length(0);
+      done();
     });
+  });
 
-    it('error.message should be {} not [object Object]', function(done) {
-        client.query('select * from objectErrorMessage', null, function(err, rows) {
-            should.exist(err);
-            err.message.should.equal('{}');
-            err.name.should.equal('ITierError');
-            should.not.exist(rows);
-            done();
-        });
+  it('error.message should be {} not [object Object]', function(done) {
+    client.query('select * from objectErrorMessage', null, function(err, rows) {
+      should.exist(err);
+      err.message.should.equal('{}');
+      err.name.should.equal('ITierError');
+      should.not.exist(rows);
+      done();
     });
+  });
 
-    it("should set `'x-itier-expire'` success", function(done) {
-        client.query('select * from myfox.table_info', null, function(err, rows, headers) {
-            should.not.exist(err);
-            rows.should.length(3);
-            rows[2].c2.should.equal('0');
-            done();
-        }, { expire: 0 });
-    });
+  it("should set `'x-itier-expire'` success", function(done) {
+    client.query('select * from myfox.table_info', null, function(err, rows, headers) {
+      should.not.exist(err);
+      rows.should.length(3);
+      rows[2].c2.should.equal('0');
+      done();
+    }, { expire: 0 });
+  });
 
-    it('should support WHERE id in (:id)', function(done) {
-        client.query('select * from hbase.test where id in (:id)', 
-        { id: [ '123', 123, 567 ] }, function(err, rows) {
-            should.not.exist(err);
-            rows.should.length(1);
-            var row = rows[0];
-            row.should.have.keys([ 'c1' ]);
-            row.c1.should.have.keys([ 'sql', 'data', 'type' ]);
-            row.c1.type.should.eql({
-                id: 'array|string'
-            });
-            done();
-        });
-    });
+  it('should support WHERE id in (:id)', function(done) {
+    client.query('select * from hbase.test where id in (:id)', 
+      { id: [ '123', 123, 567 ] }, function(err, rows) {
+                                                         should.not.exist(err);
+                                                         rows.should.length(1);
+                                                         var row = rows[0];
+                                                         row.should.have.keys([ 'c1' ]);
+                                                         row.c1.should.have.keys([ 'sql', 'data', 'type' ]);
+                                                         row.c1.type.should.eql({
+                                                           id: 'array|string'
+                                                         });
+                                                         done();
+                                                       });
+  });
 });
 
 after(function() {
-    HTTP.close();
+  HTTP.close();
 });
 
